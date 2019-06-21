b477bcdd0acaab3be6cd36d53fa50408c7bc3520
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-06-21 12:05:15 -0700

    Setup first book endpoint

--- StartHere/src/main/java/com/lambdaschool/starthere/controllers/BookController.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/controllers/BookController.java
@@ -1,7 +1,13 @@
 package com.lambdaschool.starthere.controllers;
 
+import com.lambdaschool.starthere.models.Book;
+import com.lambdaschool.starthere.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 public class BookController {
@@ -9,8 +15,12 @@ public class BookController {
     @Autowired
     BookService bookService;
 
-    @GetMapping
-    @PutMapping
-    @PostMapping
-    @DeleteMapping
+    @GetMapping("/books")
+    public ResponseEntity<?> getBooks() {
+        ArrayList<Book> books = bookService.findAll();
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+//    @PutMapping
+//    @PostMapping
+//    @DeleteMapping
 }
--- StartHere/src/main/java/com/lambdaschool/starthere/services/BookServiceImpl.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/services/BookServiceImpl.java
@@ -1,18 +1,23 @@
 package com.lambdaschool.starthere.services;
 
 import com.lambdaschool.starthere.models.Book;
+import com.lambdaschool.starthere.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
+@Service(value = "bookService")
 public class BookServiceImpl implements BookService {
-    
+
     @Autowired
     BookRepository bookRepository;
 
     @Override
     public ArrayList<Book> findAll() {
-        return null;
+        ArrayList<Book> addBooks = new ArrayList<>();
+        bookRepository.findAll().iterator().forEachRemaining(addBooks::add);
+        return addBooks;
     }
 
     @Override
