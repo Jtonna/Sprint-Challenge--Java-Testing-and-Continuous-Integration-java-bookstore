aa8f96d7c416b9fb763e9a0ba227c62b8cf7f4e4
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-06-21 11:06:35 -0700

    Added import to author controller, created service & serviceimpl

--- StartHere/src/main/java/com/lambdaschool/starthere/controllers/AuthorController.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/controllers/AuthorController.java
@@ -1,5 +1,6 @@
 package com.lambdaschool.starthere.controllers;
 
+import com.lambdaschool.starthere.services.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ StartHere/src/main/java/com/lambdaschool/starthere/services/AuthorServiceImpl.java
@@ -0,0 +1,8 @@
+package com.lambdaschool.starthere.services;
+
+import org.springframework.stereotype.Service;
+
+@Service(value = "authorService")
+public class AuthorServiceImpl {
+
+}
