611305187127e7539d0f9ed1ff0677c7daa0b5e3
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-06-21 10:41:28 -0700

    Update Author.java

--- StartHere/src/main/java/com/lambdaschool/starthere/models/Author.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/models/Author.java
@@ -1,5 +1,45 @@
 package com.lambdaschool.starthere.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class Author {
 
+    @Id
+    @GeneratedValue
+    private long authorid;
+
+    private String firstname, lastname;
+
+    public Author() {
+    }
+
+    public Author(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public long getAuthorid() {
+        return authorid;
+    }
+
+    public void setAuthorid(long authorid) {
+        this.authorid = authorid;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 }
