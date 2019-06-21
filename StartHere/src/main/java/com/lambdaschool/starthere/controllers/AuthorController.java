28f13df369ae890c845c1f87ce16c45124e1b383
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-06-21 11:20:23 -0700

    Finished the controller

--- StartHere/src/main/java/com/lambdaschool/starthere/controllers/AuthorController.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/controllers/AuthorController.java
@@ -1,10 +1,14 @@
 package com.lambdaschool.starthere.controllers;
 
+import com.lambdaschool.starthere.models.Author;
 import com.lambdaschool.starthere.services.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 public class AuthorController {
@@ -14,6 +18,7 @@ public class AuthorController {
 
     @GetMapping("/authors")
     public ResponseEntity<?> getAuthors(){
-        return null;
+        ArrayList<Author> authors = authorService.findAll();
+        return new ResponseEntity<>(authors, HttpStatus.OK);
     }
 }
