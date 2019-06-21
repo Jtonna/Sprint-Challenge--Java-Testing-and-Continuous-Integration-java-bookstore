3ae7b1a73849bddff1856d0ebd48b2dad7069da1
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-06-21 10:38:22 -0700

    Update Book.java

--- StartHere/src/main/java/com/lambdaschool/starthere/models/Book.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/models/Book.java
@@ -1,4 +1,56 @@
 package com.lambdaschool.starthere.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class Book {
+
+    @Id
+    @GeneratedValue
+    private long bookid;
+
+    private String booktitle, isbn;
+
+    private int copy;
+
+    public Book() {
+    }
+
+    public Book(String booktitle, String isbn, int copy) {
+        this.booktitle = booktitle;
+        this.isbn = isbn;
+        this.copy = copy;
+    }
+
+    public long getBookid() {
+        return bookid;
+    }
+
+    public void setBookid(long bookid) {
+        this.bookid = bookid;
+    }
+
+    public String getBooktitle() {
+        return booktitle;
+    }
+
+    public void setBooktitle(String booktitle) {
+        this.booktitle = booktitle;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getCopy() {
+        return copy;
+    }
+
+    public void setCopy(int copy) {
+        this.copy = copy;
+    }
 }
