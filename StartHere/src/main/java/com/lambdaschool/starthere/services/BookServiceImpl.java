9b3ae8fe15583f55c51a976ce777970b6b5660c4
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-06-21 12:10:57 -0700

    added impl for the controllers

--- StartHere/src/main/java/com/lambdaschool/starthere/services/BookServiceImpl.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/services/BookServiceImpl.java
@@ -1,5 +1,7 @@
 package com.lambdaschool.starthere.services;
 
+import com.lambdaschool.starthere.exceptions.ResourceNotFoundException;
+import com.lambdaschool.starthere.models.Author;
 import com.lambdaschool.starthere.models.Book;
 import com.lambdaschool.starthere.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,8 @@ public class BookServiceImpl implements BookService {
     @Autowired
     BookRepository bookRepository;
 
+    // finds all books
+
     @Override
     public ArrayList<Book> findAll() {
         ArrayList<Book> addBooks = new ArrayList<>();
@@ -20,13 +24,43 @@ public class BookServiceImpl implements BookService {
         return addBooks;
     }
 
+    // update (adding books -> authors
     @Override
     public void update(long id, Book book) {
+        Book currentBook = bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Cannot find book with the id of: " + id));
 
+        if (book.getBooktitle() != null) {
+            currentBook.setBooktitle(book.getBooktitle());
+        }
+
+        if (book.getIsbn() != null) {
+            currentBook.setIsbn(book.getIsbn());
+        }
+
+        currentBook.setCopy(book.getCopy());
+
+        if (book.getAuthors().size() != 0) {
+            currentBook.getAuthors().clear();
+            ArrayList<Author> newAuthors = new ArrayList<>();
+            for (Author author : book.getAuthors()) {
+                newAuthors.add(new Author(author.getLastname(), author.getFirstname()));
+            }
+            currentBook.setAuthors(newAuthors);
+        }
+
+        bookRepository.save(currentBook);
     }
 
+    // delete books
     @Override
     public void delete(long id) {
+        Book currentBook = bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Cannot find book with the id of: " + id));
 
+        if (currentBook != null) {
+            bookRepository.deleteById(id);
+        }
     }
+
 }
